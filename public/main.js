15814415bb1ace670ef09b1df5575276246e462b
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-04-24 21:44:21 +0200

    add main.js and center css

---
+++ public/main.js
@@ -0,0 +1,22 @@
+import { AboutView } from "./views/about.js"
+import { NotFoundView } from "./views/notFound.js"
+import { HomeView } from "./views/home.js"
+import { LoginView } from "./views/login.js"
+import { RegisterView } from "./views/register.js"
+import { ProfileView } from "./views/profile.js"
+import { LobbyView } from "./views/lobby.js"
+
+import { Router } from "./router.js"
+
+const routes = {
+    '/': LoginView,
+    '/login': LoginView,
+    '/register': RegisterView,
+    '/home': HomeView,
+    '/about': AboutView,
+    '/profile': ProfileView,
+    '/lobby': LobbyView,
+    '404': NotFoundView
+};
+
+new Router(routes);
