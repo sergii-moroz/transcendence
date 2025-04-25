4a2276291524acd8b2d53b5336c0a103ffb36a40
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-04-25 19:49:54 +0200

    tabs

--- public/router.js
+++ public/router.js
@@ -2,70 +2,65 @@ import { Api } from "./api.js"
 
 export class Router {
 
-    constructor(routes) {
-        this.routes = routes;
-        this.rootElement = document.getElementById('app');
-        this.currentView = null;
-        this.api = new Api();
-        this.publicRoutes = ['/login', '/register', '/'];
-        this.currentUser = null;
+	constructor(routes) {
+		this.routes = routes;
+		this.rootElement = document.getElementById('app');
+		this.currentView = null;
+		this.api = new Api();
+		this.publicRoutes = ['/login', '/register', '/'];
+		this.currentUser = null;
 
-        
-        this.handleRouteChange = this.handleRouteChange.bind(this);
-        this.navigateTo = this.navigateTo.bind(this);
-        
-        window.addEventListener('popstate', this.handleRouteChange);
-        document.addEventListener("click", (e) => {
-            if (e.target.matches("[data-link]")) {
-                e.preventDefault()
-                this.navigateTo(e.target.getAttribute('href'))
-                return ;
-            }
-        })
-        
-        console.log("router initialized");
-        this.handleRouteChange();
-    }
-    
-    async handleRouteChange() {
-        const path = window.location.pathname;
-        
-        if (this.currentView) {
-            this.currentView.unmount();
-            this.currentView = null;
-        }
-    
-        try {
-            if (!this.publicRoutes.includes(path)) {
-                if (!this.currentUser) {
-                    this.currentUser = await this.api.checkAuth();
-                    if (!this.currentUser) {
-                        return this.navigateTo('/login');
-                    }
-                }
-            }
-            else if (this.currentUser) {
-                return this.navigateTo('/home');
-            }
-            // error: changing url to /about from login -> error messages in browser consol
-            // error: changing url to /login from home -> able to go back (shouldn't), But changing url back to /home still works then
+		window.addEventListener('popstate', this.handleRouteChange);
+		document.addEventListener("click", (e) => {
+			if (e.target.matches("[data-link]")) {
+				e.preventDefault()
+				this.navigateTo(e.target.getAttribute('href'))
+				return ;
+			}
+		})
 
-            //--debug
-            // this.currentUser ? console.log(`user: ${this.currentUser.username}`) : console.log(`user: ${this.currentUser}`);
-            // console.log(`path: ${path}`);
-            
-            
-            const ViewClass = this.routes[path] || this.routes["404"];
-            this.currentView = new ViewClass(this.api, this);
-            await this.currentView.mount(this.rootElement);
-        } catch (error) {
-            console.error('Route handling failed:', error);
-            this.navigateTo('/login');
-        }
-    }
-    
-    navigateTo(url) {
-        window.history.pushState(null, null, url);
-        this.handleRouteChange();
-    }
+		this.handleRouteChange();
+	}
+
+	handleRouteChange = async () => {
+		const path = window.location.pathname;
+
+		if (this.currentView) {
+			this.currentView.unmount();
+			this.currentView = null;
+		}
+
+		try {
+			if (!this.publicRoutes.includes(path)) {
+				if (!this.currentUser) {
+					this.currentUser = await this.api.checkAuth();
+					if (!this.currentUser) {
+							return this.navigateTo('/login');
+					}
+				}
+			}
+			else if (this.currentUser) {
+					return this.navigateTo('/home');
+			}
+			// error: changing url to /about from login -> error messages in browser consol
+			// error: changing url to /login from home -> able to go back (shouldn't), But changing url back to /home still works then
+
+			//--debug
+			// this.currentUser ? console.log(`user: ${this.currentUser.username}`) : console.log(`user: ${this.currentUser}`);
+			// console.log(`path: ${path}`);
+
+
+			const ViewClass = this.routes[path] || this.routes["404"];
+			this.currentView = new ViewClass(this.api, this);
+			await this.currentView.mount(this.rootElement);
+		} catch (error) {
+			console.error('Route handling failed:', error);
+			this.navigateTo('/login');
+		}
+	}
+
+	navigateTo = (url) => {
+		window.history.pushState(null, null, url);
+		this.handleRouteChange();
+	}
 }
