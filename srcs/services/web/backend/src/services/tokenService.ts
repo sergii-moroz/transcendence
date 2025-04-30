53517190ff7eb033c1ce1be036e273650018ef81
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-04-30 13:23:54 +0200

    add helper services

---
+++ srcs/services/web/backend/src/services/tokenService.ts
@@ -0,0 +1,24 @@
+import crypto from 'crypto'
+import { JwtUserPayload } from '../types/user.js';
+import jwt from 'jsonwebtoken'
+
+const ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_SECRET || 'supersecret-key-supersecret-key!' as string;
+const REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_SECRET || 'refresh-secret' as string;
+
+export function generateAccessToken(user: JwtUserPayload): string {
+	return jwt.sign({ id: user.id, username: user.username }, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+	// return jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+}
+
+export function generateRefreshToken(user: JwtUserPayload): string {
+	return jwt.sign({ id: user.id, username: user.username }, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+	// return jwt.sign(user, REFRESH_TOKEN_SECRET, { expiresIn: '7d' });
+}
+
+export function createCsrfToken() {
+	return crypto.randomBytes(24).toString('hex');
+}
+
+export function verifyAccessToken(token: string): JwtUserPayload {
+	return jwt.verify(token, ACCESS_TOKEN_SECRET) as JwtUserPayload;
+}
---
+++ srcs/services/web/backend/src/services/userService.ts
@@ -0,0 +1,38 @@
+import bcrypt from 'bcrypt';
+import { User } from '../types/user.js';
+import { db } from '../db/connections.js';
+
+export const findUserById = async (id: number): Promise<User | undefined> => {
+	return new Promise((resolve, reject) => {
+		db.get<User>('SELECT username, bio FROM users WHERE id = ?', [id], (err, row) => {
+			if (err) reject(err)
+			else resolve(row)
+		})
+	})
+}
+
+export async function findUserByUsername(username: string): Promise<User | undefined> {
+	return new Promise((resolve, reject) => {
+		db.get<User>('SELECT * FROM users WHERE username = ?', [username], (err, row) => {
+			if (err) reject(err);
+			else resolve(row);
+		});
+	});
+}
+
+export async function verifyPassword(plainPassword: string, hashedPassword: string): Promise<boolean> {
+	return bcrypt.compare(plainPassword, hashedPassword);
+}
+
+export const createUser = async (username: string, hashedPassword: string): Promise<number> => {
+	return new Promise((resolve, reject) => {
+		db.run(
+			`INSERT INTO users (username, password) VALUES (?, ?)`,
+			[username, hashedPassword],
+			function (err) {
+				if (err) return reject(err);
+				resolve(this.lastID); // grab new user ID
+			}
+		);
+	});
+}
