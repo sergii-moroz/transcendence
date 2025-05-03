c84fc89bda0a3a176098d8a4b6442654c0eb25ac
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-03 21:13:48 +0200

    add new migration: 002-seed-users

---
+++ srcs/services/web/backend/src/db/migrations/002-seed-users.ts
@@ -0,0 +1,28 @@
+import { db } from "../connections.js";
+import bcrypt from 'bcrypt'
+
+export async function up() {
+	return new Promise<void>((resolve, reject) => {
+		const users = [
+			{ username: 'admin', password: 'password', bio: 'This is the admin profile.' },
+			{ username: 'smoroz', password: 'password', bio: 'smoroz likes coffee.' },
+			{ username: 'olanokhi', password: 'password', bio: 'Alex likes chocolate.' },
+			{ username: 'tecker', password: 'password', bio: 'Tom writes code.' },
+			{ username: 'dolifero', password: 'password', bio: 'Dima is developing a game.' },
+		];
+
+		db.serialize(() => {
+			for (const user of users) {
+				const hashed = bcrypt.hashSync(user.password, 10);
+				db.run(
+					`INSERT INTO users (username, password, bio) VALUES (?, ?, ?)`,
+					[user.username, hashed, user.bio],
+					(err) => {
+						if (err) reject(err);
+					}
+				);
+			}
+			resolve(); // call once after all .run() calls
+		});
+	});
+}
