11f3a8c725af0ad7cf7c4b7254a209f451e1bbaf
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-12 20:17:49 +0200

    add /2fa/verify route

--- srcs/services/web/backend/src/routes/v1/2fa.ts
+++ srcs/services/web/backend/src/routes/v1/2fa.ts
@@ -8,7 +8,19 @@ import {
 	checkCsrf
 } from "../../services/authService.js";
 
-import { updateTwoFASecret } from "../../services/2faService.js";
+import {
+	load2FASecret,
+	mark2FAVerified,
+	update2FASecret
+} from "../../services/2faService.js";
+
+import {
+	Invalid2FACodeError,
+	Missing2FACodeError,
+	SecretNotFoundError
+} from "../../errors/errors_2fa.js";
+
+import { verify2FASchema } from "../../schemas/2fa-schemas.js";
 import { JwtUserPayload } from "../../types/user.js";
 import { authenticator } from "otplib";
 import QRCode from 'qrcode'
@@ -21,11 +33,36 @@ export const twoFARoutes = async (app: FastifyInstance, opts: FastifyPluginOptio
 			const otpauth = authenticator.keyuri(user.username, 'ft_transcendence', secret);
 			const qr = await QRCode.toDataURL(otpauth);
 
-			await updateTwoFASecret(user.username, secret)
+			await update2FASecret(user.username, secret)
 
 			reply.send({ qr, secret });
 		} catch (err) {
 			reply.code(500).send({ error: 'Failed to register 2FA' });
 		}
 	});
+
+	app.post('/verify', {schema: verify2FASchema, preHandler: [authenticate, checkCsrf]}, async (req, reply) => {
+		try {
+			const user = req.user as JwtUserPayload
+			const { code } = req.body as { code?: string };
+
+			if (!code) {
+				throw new Missing2FACodeError()
+			}
+
+			const secret = await load2FASecret(user.id)
+
+			if (!secret) throw new SecretNotFoundError()
+
+			const isValid = authenticator.check(code, secret)
+
+			if (!isValid) throw new Invalid2FACodeError()
+
+			await mark2FAVerified(user.id)
+
+			reply.send({ success: true });
+		} catch (err) {
+			throw err
+		}
+	})
 }
