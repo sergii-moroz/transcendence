d25f0e32b1acb3944697de778ac3901467773071
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-12 14:54:12 +0200

    add new icon SquaraArrowUpRight

--- srcs/services/web/backend/src/public/components/icons.ts
+++ srcs/services/web/backend/src/public/components/icons.ts
@@ -9,3 +9,5 @@ export const iconShieldCheck = '<svg xmlns="http://www.w3.org/2000/svg" width="2
 export const iconUserRound = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5"><circle cx="12" cy="8" r="5"/><path d="M20 21a8 8 0 0 0-16 0"/></svg>'
 
 export const iconChevronLeft = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5"><path d="m15 18-6-6 6-6"/></svg>'
+
+export const iconSquraArrowUpRight = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/><path d="m21 3-9 9"/><path d="M15 3h6v6"/></svg>'
