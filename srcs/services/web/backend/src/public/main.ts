eecfb3ceb7614c11593d92f5470d656ca2783d6c
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-12 14:48:21 +0200

    add settings page to the main-router

--- srcs/services/web/backend/src/public/main.ts
+++ srcs/services/web/backend/src/public/main.ts
@@ -1,10 +1,11 @@
+import { WaitingView } from "./views/waitingRoom.js"
 import { NotFoundView } from "./views/notFound.js"
 import { RegisterView } from "./views/register.js"
+import { SettingsView } from "./views/settings.js"
 import { ProfileView } from "./views/profile.js"
 import { AboutView } from "./views/about.js"
 import { LoginView } from "./views/login.js"
 import { HomeView } from "./views/home.js"
-import { WaitingView } from "./views/waitingRoom.js"
 import { GameView } from "./views/game.js"
 import { RootView } from "./views/root.js"
 
@@ -20,6 +21,7 @@ const routes: Record<string, typeof View> = {
 	'/profile': ProfileView,
 	'/waiting-room': WaitingView,
 	'/game/:gameRoomId': GameView,
+	'/settings': SettingsView,
 	'404': NotFoundView
 };
 
