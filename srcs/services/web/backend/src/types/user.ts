95b5b87c3792eb580b05a0ceb88afcc40f07f9d2
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-12 20:26:16 +0200

    modify User type: - two_factor_enabled - two_factor_verified - two_factor_secret - two_factor_backup_codes

--- srcs/services/web/backend/src/types/user.ts
+++ srcs/services/web/backend/src/types/user.ts
@@ -3,6 +3,10 @@ export interface User {
 	username: string;
 	password: string;
 	bio: string;
+	two_factor_enabled: boolean;
+	two_factor_verified: boolean;
+	two_factor_secret: string;
+	two_factor_backup_codes: string;
 }
 
 export type JwtUserPayload = Pick<User, 'id' | 'username'>
