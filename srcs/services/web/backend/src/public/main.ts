ffdf7a391bc6c65b60864c2a77b19801a6804d07
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-12 21:20:40 +0200

    register completed page in frontend router

--- srcs/services/web/backend/src/public/main.ts
+++ srcs/services/web/backend/src/public/main.ts
@@ -2,6 +2,7 @@ import { VerificationMethodView } from "./views/2fa-verification-method.js"
 import { Register2FAView } from "./views/2fa-register.js"
 import { Verify2FAView } from "./views/2fa-verify.js"
 import { Backup2FAView } from "./views/2fa-backup.js"
+import { Completed2FAView } from "./views/2fa-completed.js"
 
 import { WaitingView } from "./views/waitingRoom.js"
 import { NotFoundView } from "./views/notFound.js"
@@ -31,6 +32,7 @@ const routes: Record<string, typeof View> = {
 	'/settings/2fa/register': Register2FAView,
 	'/settings/2fa/verify': Verify2FAView,
 	'/settings/2fa/backup': Backup2FAView,
+	'/settings/2fa/completed': Completed2FAView,
 	'404': NotFoundView
 };
 
