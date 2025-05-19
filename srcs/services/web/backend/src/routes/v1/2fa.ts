ad46a14be1959a74ac98b416e7c7149833f1890c
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-19 12:26:59 +0200

    add new API ROUTE: /verify-login. Checks 2FA code and generate Access, refresh, csrf tokens

--- srcs/services/web/backend/src/routes/v1/2fa.ts
+++ srcs/services/web/backend/src/routes/v1/2fa.ts
@@ -24,9 +24,17 @@ import {
 	SecretNotFoundError
 } from "../../errors/errors_2fa.js";
 
+import {
+	createCsrfToken,
+	generateAccessToken,
+	generateRefreshToken,
+	verify2FAAccessToken
+} from "../../services/tokenService.js";
+
 import { verify2FASchema } from "../../schemas/2fa-schemas.js";
 import { JwtUserPayload } from "../../types/user.js";
 import { authenticator } from "otplib";
+import { findUserById } from "../../services/userService.js";
 import QRCode from 'qrcode'
 
 export const twoFARoutes = async (app: FastifyInstance, opts: FastifyPluginOptions) => {
@@ -111,4 +119,61 @@ export const twoFARoutes = async (app: FastifyInstance, opts: FastifyPluginOptio
 		app.log.info(isEnabled, "Enabled!!!")
 		reply.send({ enabled: isEnabled })
 	});
+
+	app.post('/verify-login', async (req, reply) => {
+		// app.log.info("verify-login")
+		const { token, code } = req.body as { token: string, code: string }
+		// app.log.info(token, "TOKEN =====")
+		// app.log.info(code, "CODE")
+
+		try {
+			const user = verify2FAAccessToken(token)
+			// app.log.info(user, "USER:")
+			const userFullData = await findUserById(user.id)
+			// app.log.info(userFullData, "userFullData:")
+
+			if (!userFullData || !userFullData.two_factor_enabled) {
+				return reply.code(401).send({error: 'Unauthorized'})
+			}
+
+			// const secret = await get2FASecret(user.id)
+			userFullData.two_factor_secret
+
+			if (!userFullData.two_factor_secret) throw new SecretNotFoundError()
+
+			const isValid = authenticator.check(code, userFullData.two_factor_secret)
+
+			if (!isValid) throw new Invalid2FACodeError()
+
+			// Passed all checks -> Generate access/refresh Tokens
+			const accessToken = generateAccessToken(user);
+			const refreshToken = generateRefreshToken(user);
+			const csrfToken = createCsrfToken();
+
+			return reply
+				.setCookie('token', accessToken, {
+					httpOnly: true,
+					secure: false, // set to true in production with HTTPS // process.env.NODE_ENV === 'production'
+					sameSite: 'strict',
+					path: '/',
+					maxAge: 60 * 15 // 15 min
+				})
+				.setCookie('refreshToken', refreshToken, {
+					httpOnly: true,
+					sameSite: 'strict',
+					path: '/',
+					maxAge: 60 * 60 * 24 * 7 // 7 days
+				})
+				.setCookie('csrf_token', csrfToken, {
+					httpOnly: false,
+					sameSite: 'strict',
+					path: '/',
+					maxAge: 60 * 15
+				})
+				.send({ success: true });
+
+		} catch (err) {
+			throw err
+		}
+	})
 }
