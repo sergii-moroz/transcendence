cdcfe452324a70d91b1e8f7b082fed7e3868ed6b
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-21 21:29:03 +0200

    add home.html template, small changes

--- srcs/services/web/backend/src/public/main-new.ts
+++ srcs/services/web/backend/src/public/main-new.ts
@@ -1,8 +1,10 @@
 import { ButtonThemeToggle } from "./components/ButtonThemeToggle.js";
 import { ModalLoginMenu } from "./components/ModalLoginMenu.js";
+import { RegisterForm } from "./components/register-form.js";
 import { Router } from "./router-static.js";
 
 customElements.define('modal-login-menu', ModalLoginMenu)
 customElements.define('btn-theme-toggle', ButtonThemeToggle)
+customElements.define('register-form', RegisterForm)
 
 Router.init()
