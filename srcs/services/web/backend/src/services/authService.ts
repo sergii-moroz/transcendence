c3449f7d69eedab7709c0c0e121bb2d925a6992e
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-21 23:46:42 +0200

    add function for server-side password validation during registration

--- srcs/services/web/backend/src/services/authService.ts
+++ srcs/services/web/backend/src/services/authService.ts
@@ -1,5 +1,38 @@
+import {
+	UsernameTooLongError,
+	UsernameTooShortError,
+	UsernameWhitespaceError,
+	PasswordTooLongError,
+	PasswordTooShortError,
+	PasswordWhitespaceError,
+	PasswordMissingUppercaseError,
+	PasswordMismatchError,
+} from "../errors/registration.js";
+
 import { FastifyReply, FastifyRequest } from "fastify";
 import { verifyAccessToken } from "./tokenService.js";
+import { RegisterInputProps } from "../types/registration.js";
+
+export const validateRegisterInput = (input: RegisterInputProps): RegisterInputProps => {
+	let { username, password, repeated } = input
+
+	username.trim()
+	password.trim()
+	repeated.trim()
+
+	if (username.length < 5) throw new UsernameTooShortError()
+	if (username.length > 15) throw new UsernameTooLongError()
+	if (/\s+/g.test(username)) throw new UsernameWhitespaceError()
+
+	if (password.length < 6) throw new PasswordTooShortError();
+	if (password.length > 64) throw new PasswordTooLongError();
+	if (/\s+/g.test(password)) throw new PasswordWhitespaceError();
+	if (!/[A-Z]/.test(password)) throw new PasswordMissingUppercaseError();
+
+	if (password != repeated) throw new PasswordMismatchError()
+
+	return { username, password, repeated }
+}
 
 export const checkCsrf = async (
 	request:FastifyRequest,
