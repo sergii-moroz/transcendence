57f1bfb41a274559480ee46ca2926db8175e92b0
dolifero <dimasikolif@gmail.com>
2025-05-24 00:58:00 +0200

    fix some issues(i guess)

--- srcs/services/web/backend/src/routes/v1/routes.ts
+++ srcs/services/web/backend/src/routes/v1/routes.ts
@@ -7,7 +7,4 @@ export const routes = async (app: FastifyInstance, opts: FastifyPluginOptions) =
 	app.get('/game/:gameRoomId', (req, reply) => {
 		return reply.type('text/html').sendFile('../public/index.html');
 	});
-	app.get('/tournament/:tournamentId', (req, reply) => {
-		return reply.type('text/html').sendFile('../public/index.html');
-	});
 }
