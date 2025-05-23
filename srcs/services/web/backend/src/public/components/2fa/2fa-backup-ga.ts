01f1fe989418fee09a6ae117f09713d7c6514e12
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-05-23 23:07:01 +0200

    add business logic to the TwoFABackupGA component

--- srcs/services/web/backend/src/public/components/2fa/2fa-backup-ga.ts
+++ srcs/services/web/backend/src/public/components/2fa/2fa-backup-ga.ts
@@ -1,3 +1,7 @@
+import { API } from "../../api-static.js"
+import { Router } from "../../router-static.js"
+import { generateBackupCodesImage } from "../../utils/utils.js"
+
 const innerHTML = `
 	<!-- CARD -->
 	<div class="tw-card w-full p-4 space-y-4
@@ -26,14 +30,18 @@ const innerHTML = `
 			<div
 				id="codes"
 				class="grid grid-cols-2 gap-4 bg-gray-500/10 text-xs text-gray-600 px-4 py-2 rounded-sm
-					border border-gray-200 dark:text-gray-200 dark:border-gray-700"
+					border border-gray-200 dark:text-gray-200 dark:border-gray-700 hidden"
 			>
-				CODES HERE
+				CODES
 			</div>
+			<p
+				class="text-red-500 text-xs text-left mt-1 hidden"
+				id="message-error"
+			></p>
 
 			<div>
 				<button id="btn-print" class="tw-btn-outline">Print</button>
-				<button id="btn-download" class="tw-btn-outline">Download (PDF)</button>
+				<button id="btn-download" class="tw-btn-outline">Download</button>
 			</div>
 		</div>
 
@@ -46,12 +54,114 @@ const innerHTML = `
 `
 
 export class TwoFABackupGA extends HTMLElement {
+	private btnNext: HTMLButtonElement | null = null
+	private btnPrint: HTMLButtonElement | null = null
+	private btnDownload: HTMLButtonElement | null = null
+	private codesContainer: HTMLElement | null = null
+	private messageError: HTMLElement | null = null
+	private backupCodes: string[] = []
+
 	constructor() {
 		super()
 		this.render()
 	}
 
+	async connectedCallback() {
+		this.btnNext = this.querySelector('#btn-next')
+		this.btnPrint = this.querySelector('#btn-print')
+		this.btnDownload = this.querySelector('#btn-download')
+		this.codesContainer = this.querySelector('#codes')
+		this.messageError = this.querySelector('#message-error')
+
+		this.btnPrint?.addEventListener('click', this.handlePrint)
+		this.btnDownload?.addEventListener('click', this.handleDownload)
+		this.btnNext?.addEventListener('click', this.handleNext)
+
+		const is2FAEnabled = await this.is2FAEnabled()
+
+		is2FAEnabled
+			? Router.navigateTo('/settings/2fa/already-enabled')
+			: await this.loadBackupCodes()
+	}
+
+	disconnectedCollback() {
+		this.btnNext?.removeEventListener('click', this.handleNext)
+		this.btnPrint?.removeEventListener('click', this.handlePrint)
+		this.btnDownload?.removeEventListener('click', this.handleDownload)
+	}
+
+	private async loadBackupCodes() {
+		if (!this.codesContainer || !this.messageError) return
+
+		const res = await API.create2FABackupCodes()
+
+		if (res.success) {
+			this.backupCodes = res.codes
+			this.codesContainer.innerHTML = this.backupCodes.map(code => `<p>${code}</p>`).join('')
+			this.codesContainer.classList.remove('hidden')
+		} else {
+			this.showError(this.messageError, res.message ?? 'Server error. Please refresh or try again later')
+		}
+	}
+
 	private render() {
 		this.innerHTML = innerHTML
 	}
+
+	private enableBtnNext() {
+		if (!this.btnNext) return
+
+		this.btnNext.disabled = false
+		this.btnNext.classList.remove('tw-btn-disabled')
+		this.btnNext.classList.add('tw-btn')
+	}
+
+	private handlePrint = () => {
+		window.print()
+		this.enableBtnNext()
+	}
+
+	private handleDownload = () => {
+		if (this.backupCodes.length === 0) return
+
+		generateBackupCodesImage(this.backupCodes)
+		this.enableBtnNext()
+	}
+
+	private handleNext() {
+		Router.navigateTo('/settings/2fa/ga/completed')
+	}
+
+	private async is2FAEnabled() {
+		if (!this.messageError) return
+
+		const res = await API.is2FAEnabled()
+
+		if (res.success) {
+			return (res.enabled)
+		} else {
+			this.showError(this.messageError, res.message ?? 'Server error. Please refresh or try again later')
+		}
+
+		return false
+	}
+
+	private showError(element: HTMLElement, message: string) {
+		let errorTimeout
+
+		clearTimeout(errorTimeout)
+
+		element.innerHTML = `
+		<div class="relative">
+			<p class="py-1">${message}</p>
+			<div class="absolute bottom-0 left-0 h-1 bg-red-300 animate-error-bar w-full rounded-sm"></div>
+		</div>
+		`
+		element.classList.remove('hidden')
+		errorTimeout = setTimeout(() => {
+			element.innerHTML = ''
+			element.classList.add('hidden')
+		}, 4000)
+	}
+
 }
