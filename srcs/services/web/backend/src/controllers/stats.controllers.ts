3fcff8ca4ea9d3fc335f99ffb25665df3f0fb695
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-06-02 10:58:52 +0200

    add: controller handleGetTopPlayer

--- srcs/services/web/backend/src/controllers/stats.controllers.ts
+++ srcs/services/web/backend/src/controllers/stats.controllers.ts
@@ -1,6 +1,11 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { JwtUserPayload } from "../types/user.js";
-import { getUserPerformance } from "../services/stats.services.js";
+
+import {
+	getMultiTopPlayers,
+	getTournamentTopPlayers,
+	getUserPerformance
+} from "../services/stats.services.js";
 
 export const handleGetUserPerformance = async (
 	req:		FastifyRequest,
@@ -8,9 +13,22 @@ export const handleGetUserPerformance = async (
 ) => {
 	try {
 		const user = req.user as JwtUserPayload
-		console.log("handleGetUserPerformance:", user)
 		const stats = await getUserPerformance(user.id)
 		reply.send(stats)
+	} catch (err) {
+		throw err
+	}
+}
+
+export const handleGetTopPlayers = async (
+	req:		FastifyRequest,
+	reply:	FastifyReply
+) => {
+	try {
+		const user = req.user as JwtUserPayload
+		const m_topPlayers = await getMultiTopPlayers()
+		const t_topPlayers = await getTournamentTopPlayers()
+		reply.send({ "singleplayer": null, "multiplayer": m_topPlayers, "tournament": t_topPlayers })
 	} catch (err) {
 		throw err
 	}
