efccd8e7898d96308109a2a2ff5afcb1e9502631
dolifero <dolifero@student.42heilbronn.de>
2025-06-02 16:15:56 +0200

    fix typo & bug

--- srcs/services/web/backend/src/routes/v1/tournamentList.ts
+++ srcs/services/web/backend/src/routes/v1/tournamentList.ts
@@ -11,50 +11,54 @@ export const tWaitingRoomSock = async (app: FastifyInstance) => {
 	const tWaitingRoomConns = new Array<[string, WebSocket]>();
 
 	app.get('/tournament-list', {websocket: true }, async (socket, req) => {
-			let userId: string = req.user.id.toString();
+		let userId: string = req.user.id.toString();
 
-			if(tWaitingRoomConns.findIndex(([id]) => id === userId) === -1) {
-				tWaitingRoomConns.push([userId!, socket]);
-				console.custom('INFO', `${userId} has joined the tournament waiting room`);
-			} else {
-				const index = tWaitingRoomConns.findIndex(([id]) => id === userId);
-				tWaitingRoomConns[index][1] = socket;
-				console.custom('INFO', `${userId} is already in the tournament waiting room, socket updated`);
+		if(tWaitingRoomConns.findIndex(([id]) => id === userId) === -1) {
+			tWaitingRoomConns.push([userId!, socket]);
+			console.custom('INFO', `${userId} has joined the tournament waiting room`);
+		} else {
+			const index = tWaitingRoomConns.findIndex(([id]) => id === userId);
+			tWaitingRoomConns[index][1] = socket;
+			console.custom('INFO', `${userId} is already in the tournament waiting room, socket updated`);
+		}
+		console.custom('INFO', 'Users in tournament waiting room:', tWaitingRoomConns.map(player => player[0]));
+
+		sendTournamentList(tWaitingRoomConns, app);
+		
+		socket.on('message', (messageBuffer: Event) => {
+			const message = JSON.parse(messageBuffer.toString());
+
+			if(message.type === 'createTournament') {
+				const tournament = new Tournament(app, message.maxPlayers || 4);
+				app.tournaments.set(tournament.id, tournament);
+				console.custom('INFO', `Tournament created with ID: ${tournament.id}`);
+				redirectToTournament(tournament.id, app, {id: userId, socket: socket}, tWaitingRoomConns);
+				setTimeout(() => {
+					sendTournamentList(tWaitingRoomConns, app);
+				}, 500);
 			}
-			console.custom('INFO', 'Users in tournament waiting room:', tWaitingRoomConns.map(player => player[0]));
 
-			sendTournamentList(tWaitingRoomConns, app);
-			
-			socket.on('message', (messageBuffer: Event) => {
-				const message = JSON.parse(messageBuffer.toString());
-
-				if(message.type === 'createTournament') {
-					const tournament = new Tournament(app, message.maxPlayers || 4);
-					app.tournaments.set(tournament.id, tournament);
-					console.custom('INFO', `Tournament created with ID: ${tournament.id}`);
-					redirectToTournament(tournament.id, app, {id: userId, socket: socket}, tWaitingRoomConns);
+			if(message.type === 'joinTournament') {
+				console.custom('INFO', `User ${userId} is joining tournament: ${message.tournamentId}`);
+				const tournamentId = message.tournamentId;
+				redirectToTournament(tournamentId, app, {id: userId, socket: socket}, tWaitingRoomConns);
+				setTimeout(() => {
 					sendTournamentList(tWaitingRoomConns, app);
-				}
+				}, 500);
+			}
+		})
 
-				if(message.type === 'joinTournament') {
-					console.custom('INFO', `User ${userId} is joining tournament: ${message.tournamentId}`);
-					const tournamentId = message.tournamentId;
-					redirectToTournament(tournamentId, app, {id: userId, socket: socket}, tWaitingRoomConns);
-					sendTournamentList(tWaitingRoomConns, app);
-				}
-			})
-	
-			socket.on('close', () => {
-				const index = tWaitingRoomConns.findIndex(([id]) => id === userId);
-				if (index !== -1) {
-					tWaitingRoomConns.splice(index, 1);
-				}
-			})
-	
-			socket.on('error', (err: Event) => {
-				console.custom('error', err);
-			})
-		});
+		socket.on('close', () => {
+			const index = tWaitingRoomConns.findIndex(([id]) => id === userId);
+			if (index !== -1) {
+				tWaitingRoomConns.splice(index, 1);
+			}
+		})
+
+		socket.on('error', (err: Event) => {
+			console.custom('error', err);
+		})
+	});
 }
 
 function redirectToTournament(tournamentId: string, app: FastifyInstance,
@@ -82,7 +86,7 @@ function sendTournamentList(connections: Array<[string, WebSocket]>, app: Fastif
 	let tournamentList = Array.from(app.tournaments.values()).map(tournament => ({
 		id: tournament.id,
 		maxPlayers: tournament.maxPlayers,
-		playersCount: tournament.players.length,
+		playerCount: tournament.players.length,
 	}));
 	connections.forEach(([userId, socket]) => {
 		socket.send(JSON.stringify({
