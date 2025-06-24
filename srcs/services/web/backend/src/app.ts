97eff69d8db097d1162417a47a346567e4f1d912
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-06-24 13:36:39 +0200

    add hook again. add mac package.json

--- srcs/services/web/backend/src/app.ts
+++ srcs/services/web/backend/src/app.ts
@@ -24,6 +24,7 @@ import { statsRoutes } from "./routes/v1/stats.routes.js";
 import { historyRoutes } from "./routes/v1/game-history.route.js";
 import { profile } from "./routes/v1/profile.js";
 import fastifyMultipart from "@fastify/multipart";
+import { verifyAccessToken } from "./services/tokenService.js";
 
 export const build = async (opts: FastifyServerOptions) => {
 	const app = fastify(opts)
@@ -55,25 +56,25 @@ export const build = async (opts: FastifyServerOptions) => {
 	const __filename = fileURLToPath(import.meta.url);
 	const __dirname = path.dirname(__filename);
 
-	// app.addHook('preValidation', async (request: FastifyRequest, reply: FastifyReply) => {
-	// 	const requestURL = request.url;
-	// 	const publicRoutes = ['/api/login', '/api/register', '/api/2fa/verify-login'];
-	// 	if ((!requestURL.startsWith('/api/') && !requestURL.startsWith('/ws/')) || publicRoutes.includes(requestURL)) {
-	// 		console.custom('DEBUG', "No authentification required for this route");
-	// 		return;
-	// 	}
-	// 	console.custom('DEBUG', 'Authentifying user...');
-	// 	const token = request.cookies.token;
-	// 	if (!token) {
-	// 		return reply.code(401).send({ type: 'error', message: 'Unauthorized: No token provided' });
-	// 	}
+	app.addHook('preValidation', async (request: FastifyRequest, reply: FastifyReply) => {
+		const requestURL = request.url;
+		const publicRoutes = ['/api/login', '/api/register', '/api/2fa/verify-login'];
+		if ((!requestURL.startsWith('/api/') && !requestURL.startsWith('/ws/')) || publicRoutes.includes(requestURL)) {
+			console.custom('DEBUG', "No authentification required for this route");
+			return;
+		}
+		console.custom('DEBUG', 'Authentifying user...');
+		const token = request.cookies.token;
+		if (!token) {
+			return reply.code(401).send({ type: 'error', message: 'Unauthorized: No token provided' });
+		}
 
-	// 	try {
-	// 		request.user = verifyAccessToken(token);
-	// 	} catch (err) {
-	// 		return reply.code(401).send({ type: 'error', message: 'Invalid or expired token' });
-	// 	}
-	// })
+		try {
+			request.user = verifyAccessToken(token);
+		} catch (err) {
+			return reply.code(401).send({ type: 'error', message: 'Invalid or expired token' });
+		}
+	})
 
 	app.setNotFoundHandler((request, reply) => {
 		const requestURL = request.url;
