88a63ae468ca133e7012b4a1cfe6df4cff0467f0
oliferovych <dimasikolif@gmail.com>
2025-07-03 14:50:58 +0200

    #47 fixed frontend diff

--- srcs/services/web/backend/src/public/components/match-making.ts
+++ srcs/services/web/backend/src/public/components/match-making.ts
@@ -55,16 +55,20 @@ export class Matchmaking extends HTMLElement {
 		console.error('WebSocket error:', err);
 	}
 
+	// <h2 class="text-xl font-semibold mb-4 text-gray-800 dark:text-white">
+	// 	Waiting for other players to join
+	// </h2>
+	// <div class="flex justify-center space-x-1 mt-2">
+	// 	<div class="h-2 w-2 bg-gray-600 dark:bg-gray-300 rounded-full animate-bounce [animation-delay:0ms]"></div>
+	// 	<div class="h-2 w-2 bg-gray-600 dark:bg-gray-300 rounded-full animate-bounce [animation-delay:150ms]"></div>
+	// 	<div class="h-2 w-2 bg-gray-600 dark:bg-gray-300 rounded-full animate-bounce [animation-delay:300ms]"></div>
+	// </div>
 	render() {
 		this.innerHTML = `
 			<div class="tw-card px-6 py-11 w-full max-w-md text-center">
-				<h2 class="text-xl font-semibold mb-4 text-gray-800 dark:text-white">
-					Waiting for other players to join
-				</h2>
-				<div class="flex justify-center space-x-1 mt-2">
-					<div class="h-2 w-2 bg-gray-600 dark:bg-gray-300 rounded-full animate-bounce [animation-delay:0ms]"></div>
-					<div class="h-2 w-2 bg-gray-600 dark:bg-gray-300 rounded-full animate-bounce [animation-delay:150ms]"></div>
-					<div class="h-2 w-2 bg-gray-600 dark:bg-gray-300 rounded-full animate-bounce [animation-delay:300ms]"></div>
+				<div class="flex flex-row items-center justify-center gap-3">
+					<div class="animate-spin rounded-full h-4 w-4 border-t-2 border-b-2 border-black-400 dark:border-white-400"></div>
+					<p id="waiting-message" class="text-sm text-black-400 dark:text-white-400 text-center m-0">Waiting for other players...</p>
 				</div>
 			</div>
 		`;
