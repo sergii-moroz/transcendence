82306be35c5e03a9feed30aa350983d86df0be96
t-ecker <152566019+t-ecker@users.noreply.github.com>
2025-07-11 16:00:18 +0200

    add link to profile to leaderboard components

--- srcs/services/web/backend/src/public/components/home/top-player-card.ts
+++ srcs/services/web/backend/src/public/components/home/top-player-card.ts
@@ -83,7 +83,7 @@ export class TopPlayerCard extends HTMLElement {
 					${iconHomeTrophy}
 				</div>
 				<div class="flex-1">
-					<div class="font-medium">${item.username}</div>
+					<a href='/profile/${item.username}' data-link class="font-medium hover:underline transition-all cursor-pointer">${item.username}</a>
 					<div class="text-xs dark:text-gray-400 text-gray-500">
 						${item.wins} wins • ${total} matches
 					</div>
