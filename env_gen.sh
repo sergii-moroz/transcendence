69fe73edb168a877628686125bacd1b3c54c7458
Alex Anokhin <olanokhi@student.42heilbronn.de>
2025-07-15 10:25:16 +0200

    Update: envgen script to gen rand keys; Fix: landing page 3d game description;

--- env_gen.sh
+++ env_gen.sh
@@ -1,10 +1,10 @@
 touch ./.env
 
-echo 'JWT_ACCESS_SECRET=supersecret-key-supersecret-key!' > ./.env
-echo 'JWT_REFRESH_SECRET=supersecret-key-supersecret-key!' >> ./.env
-echo 'JWT_2FA_ACCESS_SECRET=supersecret-key-supersecret-key!' >> ./.env
-echo 'HOST=0.0.0.0' >> ./.env
-echo 'PORT=443' >> ./.env
+echo "JWT_ACCESS_SECRET=$(openssl rand -hex 16)" > ./.env
+echo "JWT_REFRESH_SECRET=$(openssl rand -hex 16)" >> ./.env
+echo "JWT_2FA_ACCESS_SECRET=$(openssl rand -hex 16)" >> ./.env
+echo "HOST=0.0.0.0" >> ./.env
+echo "PORT=443" >> ./.env
 
 echo '' >> ./.env
 echo 'ELASTIC_PASSWORD=kibanapass' >> ./.env
